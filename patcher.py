4c53b1362e2472191189d0f8cbf663bde67ed18e
Vicente Olivert Riera <vincent.olivert.riera@gmail.com>
2023-02-11 11:09:49 +0900

    Update to latest undetected_chromedriver version
    The new version was giving some problems when patching not up-to-date
    chromedrivers. To fix that, the `main_version` needs to be passed.

--- patcher.py
+++ patcher.py
@@ -1,7 +1,15 @@
 import os
 import shutil
+import subprocess
 
 import undetected_chromedriver as uc
+
+result = subprocess.run(
+    ["/usr/bin/chromedriver", "--version"],
+    capture_output=True,
+    text=True
+)
+chromedriver_version = result.stdout.split(".")[0].split(" ")[1]
 
 chromedriver_copy_path = f"{os.getcwd()}/chromedriver"
 shutil.copy("/usr/bin/chromedriver", chromedriver_copy_path)
@@ -9,5 +17,9 @@ shutil.copy("/usr/bin/chromedriver", chromedriver_copy_path)
 options = uc.ChromeOptions()
 options.add_argument("--headless")
 
-driver = uc.Chrome(driver_executable_path=chromedriver_copy_path, options=options)
+driver = uc.Chrome(
+    driver_executable_path=chromedriver_copy_path,
+    options=options,
+    version_main=chromedriver_version
+)
 driver.get("chrome://newtab")
