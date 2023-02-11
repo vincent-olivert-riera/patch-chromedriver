ba2f6d8a77cfdd650f9810cd908e75a60ef2585a
Vicente Olivert Riera <vincent.olivert.riera@gmail.com>
2023-02-11 11:08:32 +0900

    patcher.py: Fix headless option
    The option is deprecated and is recommended to be passed using add_argument.
    Also, replace some single quotes with double quotes.

--- patcher.py
+++ patcher.py
@@ -7,8 +7,7 @@ chromedriver_copy_path = f"{os.getcwd()}/chromedriver"
 shutil.copy("/usr/bin/chromedriver", chromedriver_copy_path)
 
 options = uc.ChromeOptions()
-options.headless=True
-options.add_argument('--headless')
+options.add_argument("--headless")
 
 driver = uc.Chrome(driver_executable_path=chromedriver_copy_path, options=options)
 driver.get("chrome://newtab")
